24260f45ae627c7e5dde594aa391829d41a6ba96
Sp-ye <a64220797@163.com>
2020-03-04 11:32:19 +0800

    支持promise的 confirm方法组件

--- src/components/Confirm/index.js
+++ src/components/Confirm/index.js
@@ -3,18 +3,9 @@ import ReactDOM from 'react-dom'
 import Modal from '../Modal'
 import Icon from '../Icon'
 import MyButton from '../MyButton'
-import PropTypes, { func } from 'prop-types'
+import PropTypes from 'prop-types'
 import './index.scss'
-let p = new Promise((resolve,reject) => {
-  function handler(e) {
-    console.log(this)
-    // if (somethingRight) {
-    //     resolve(e);
-    // } else {
-    //     reject(e);
-    // }
-  }
-})
+
 
 
 export class Confirm extends Component {
@@ -34,7 +25,6 @@ export class Confirm extends Component {
   show = () => {
     console.log('show')
     this.setState({ visible: true })
-    handler()
     
   }
   cancel = () => {
@@ -45,8 +35,7 @@ export class Confirm extends Component {
     this.setState({ visible: false })
   }
   componentDidMount() {
-    console.log(22222)
-    this.show()
+    // this.show()
   }
   render() {
     if(!this.state.visible) return null;
@@ -55,17 +44,15 @@ export class Confirm extends Component {
         <div className="my-confirm">
           <div className="my-confirm__content">
             <MyButton className="my-confirm__close">
-              <span className="my-confirm__close__x">
-                <Icon name='icon-close'/>
-              </span>
+              <span className="my-confirm__close__x"><Icon name='icon-close'/></span>
             </MyButton>
             <div className="my-confirm__header">
               <div className="my-confirm__title">{this.props.title}</div>
             </div>
             <div className="my-confirm__body"><p>{this.props.msg}</p></div>
             <div className="my-confirm__footer">
-              <MyButton className="my-confirm__footer__cancelBtn" title="取消" onClick={this.cancel}/>
-              <MyButton title="确定" type="primary" onClick={this.confirm}/>
+              <MyButton id="my-confirm__footer__cancelBtn" className="my-confirm__footer__cancelBtn" title="取消" onClick={this.cancel}/>
+              <MyButton id="my-confirm__footer__confirmBtn" title="确定" type="primary" onClick={this.confirm}/>
             </div>
           </div>
         </div>
@@ -74,15 +61,24 @@ export class Confirm extends Component {
   }
 }
 
+// 应该不需要我这么麻烦吧....
 let node = null
 export default async function confirm(msg) {
+  let p
   node = document.createElement('div')
   document.body.appendChild(node)
   await ReactDOM.render(<Confirm msg={msg}/>, node, function() {
-    console.log(11111)
-    
+    p = new Promise((resolve,reject) => {
+      const cancelBtn = document.getElementById("my-confirm__footer__cancelBtn")
+      const confirmBtn = document.getElementById("my-confirm__footer__confirmBtn")
+      cancelBtn.addEventListener('click', () => {
+        reject('取消')
+      })
+      confirmBtn.addEventListener('click', () => {
+        resolve('确认')
+      })
+    }).catch( e => {
+    })
   })
-  await console.log(2222)
-  console.log(p)
   return p
 }
